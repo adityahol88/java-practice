09e8d024fff9240a293d20377c2817b9f3f7767a
Aditya Hol <adityahol88@gmail.com>
2026-07-31 11:22:01 +0530

    Frequency Count

---
+++ src/collections/FrequencyCount.java
@@ -0,0 +1,43 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class FrequencyCount {
+	
+	
+	public static void frequencyCount(ArrayList<Integer> list) {
+		HashSet<Integer> done=new HashSet<>();
+		for (int i = 0; i < list.size(); i++) {
+			Integer cur = list.get(i);
+		    if (done.contains(cur)) {
+		        continue;
+		    }
+		    int count = 1;
+			for(int j=i+1;j<list.size();j++) {
+				if(list.get(j).equals(cur)) {
+					count++;
+			}
+			}
+			System.out.println("Frequency of element "+ cur +" is "+ count);
+			done.add(cur);
+		}
+}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		System.out.print("Enter number of elements: ");
+		int n = sc.nextInt();
+
+		System.out.println("Enter " + n + " integers:");
+
+		for (int i = 0; i < n; i++) {
+		    list.add(sc.nextInt());
+		}
+		frequencyCount(list);
+		sc.close();
+	}
+}
