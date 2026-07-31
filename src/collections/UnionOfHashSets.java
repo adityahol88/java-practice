33779a13d6a4c85b332694b20262bf0aec255432
Aditya Hol <adityahol88@gmail.com>
2026-07-31 11:54:07 +0530

    Union of two hashSets

--- src/collections/UnionOfHashSets.java
+++ src/collections/UnionOfHashSets.java
@@ -1,7 +1,36 @@
 package collections;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class UnionOfHashSets {
 	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+
+		HashSet<Integer> set1 = new HashSet<>();
+		HashSet<Integer> set2 = new HashSet<>();
+
+		System.out.print("Enter number of elements in first set : ");
+		int n1 = sc.nextInt();
+
+		System.out.println("Enter " + n1 + " integers:");
+		for (int i = 0; i < n1; i++) {
+		    set1.add(sc.nextInt());
+		}
+		System.out.print("Enter number of elements in second set : ");
+		int n2 = sc.nextInt();
+
+		System.out.println("Enter " + n2 + " integers:");
+		for (int i = 0; i < n2; i++) {
+		    set1.add(sc.nextInt());
+		}
+		HashSet<Integer> union = new HashSet<>(set1);
+		union.addAll(set2);
+		System.out.println("Union of two sets is ; ");
+		System.out.println(union);
+		sc.close();
 		
 	}
 }
