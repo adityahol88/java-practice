4d8993d3b837472808859df467dcece6bf683b74
Aditya Hol <adityahol88@gmail.com>
2026-08-03 09:30:32 +0530

    Lambda Expressions - Empty string check

--- src/lambda_expresssions/IsStringEmpty.java
+++ src/lambda_expresssions/IsStringEmpty.java
@@ -1,8 +1,22 @@
 package lambda_expresssions;
 
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class IsStringEmpty {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter string to check - ");
+
+		String str = sc.nextLine();
+		Predicate<String> isEmpty = string -> string.isEmpty();
+		if(isEmpty.test(str)) {
+			System.out.println("Given string is Empty");
+		}
+		else {
+			System.out.println("Given string is not empty ");
+		}
+sc.close();
 	}
 }
